1a55c6662f8a0a1cbf373853ef7c9b6bdf12c84d
Brad Schwartz <35202550+nicschwartz@users.noreply.github.com>
2022-05-02 12:19:04 -0700

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,41 @@
+//This code will be included as the 'footer' of your web page content. 
+//Include things like <footer> or anything else that should appear at the top of very web page on your site. 
+  
+<footer>
+  
+<article>
+
+<ul>
+    <li><a href="#">Item 1</a></li>
+    <li><a href="#">Item 2</a></li>
+    <li><a href="#">Item 3</a></li>
+    <li><a href="#">Item 4</a></li>
+</ul>
+
+</article>
+
+<article>
+
+<ul>
+    <li><a href="#">Item 1</a></li>
+    <li><a href="#">Item 2</a></li>
+    <li><a href="#">Item 3</a></li>
+    <li><a href="#">Item 4</a></li>
+</ul>
+
+</article>
+
+<article>
+
+<ul>
+    <li><a href="#">Item 1</a></li>
+    <li><a href="#">Item 2</a></li>
+    <li><a href="#">Item 3</a></li>
+    <li><a href="#">Item 4</a></li>
+</ul>
+
+</article>
+
+    
+</footer>
+  
