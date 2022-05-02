6f018c2fc8bd9d2585a94f5b83d679b04a5fa019
Brad Schwartz <35202550+nicschwartz@users.noreply.github.com>
2022-05-02 12:17:20 -0700

    Create header.php

---
+++ header.php
@@ -0,0 +1,15 @@
+//This code will be included as the 'header' of your web page content. 
+//Include things like <header> and <nav> elements or anything else that should appear at the top of very web page on your site. 
+  
+<header>
+  
+<nav>
+  <ul>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">About</a></li>
+    <li><a href="#">Projects</a></li>
+    <li><a href="#">Contact</a></li>
+</nav>
+  
+</header>
+  
