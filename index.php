3cb47eccb22a56e600f76fdd7cde996a47d001c9
Brad Schwartz <35202550+nicschwartz@users.noreply.github.com>
2022-05-02 12:15:15 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+// Follow the guidelines here for using includes or require functions
+// https://www.tutorialrepublic.com/php-tutorial/php-include-files.php
+//the following uses the HTML5 Boilerplate from VS Code
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>PHP Includes</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+<?php include "header.php"; ?>
+
+      
+<?php include "footer.php"; ?>
+
+  </body>
+</html>
