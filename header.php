464f6d513c71f79affe5a3e953eecfb5e44507ab
Brad Schwartz <35202550+nicschwartz@users.noreply.github.com>
2022-05-02 12:38:39 -0700

    Update header.php

--- header.php
+++ header.php
@@ -1,6 +1,8 @@
+<!--
 //This code will be included as the 'header' of your web page content. 
 //Include things like <header> and <nav> elements or anything else that should appear at the top of very web page on your site. 
-  
+-->
+
 <header>
   
 <nav>
